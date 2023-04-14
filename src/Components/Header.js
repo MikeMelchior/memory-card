7c6312142820569800b49e25eea588465cff3088
Mike Melchior <mike.melchior@outlook.com>
2023-04-14 12:33:53 -0400

    add logo to header

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../paw.png'
+
+export default function Header() {
+
+  return (
+    <header>
+      <img id="logo" src={logo} alt="logo" />
+
+    </header>
+  )
+}
