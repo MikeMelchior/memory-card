bf12a3bce1d51e9eeeacec7393d3b5a4ead59f19
Mike Melchior <mike.melchior@outlook.com>
2023-04-17 12:55:42 -0400

    add components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import Game from './Components/Game'
+import Header from "./Components/Header";
+import './styles.css'
 
 function App() {
+
+  
+
   return (
-    <div></div>
+    <>
+    <Header />
+    <Game />
+    </>
+    
   );
 }
 
