00ae1c47ef5baa020d3f82a6f370e7053a6b032f
Mike Melchior <mike.melchior@outlook.com>
2023-04-17 17:09:32 -0400

    give card ability to add point to score

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react"
+import React from "react"
 
-export default function Card({ url, shuffle, card, setLoss }) {
+export default function Card({ url, shuffle, card, setLoss, setScore }) {
   
   const handleClick = () => {
     console.log(card.clicked)
     if(card.clicked === true) {
       setLoss(true)
+      return;
     }
+    setScore(currScore => currScore + 1)
     card.clicked = true
   }
 
